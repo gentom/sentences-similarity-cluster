3f4128f5bbe4f587432e3ca1fb85c567f605967f
gentom <gento.m.developer@gmail.com>
2017-09-28 22:40:44 +0900

    levenshtein distance

---
+++ levenshteinDistance.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+
+def editDistance(s1, s2):
+    if len(s1) > len(s2):
+        s1, s2 = s2, s1
+
+    # リストの値はid番号で管理する
+    #print('{}, {}'.format(s1,s2))
+    distances = range(len(s1) + 1)
+    for i2, c2 in enumerate(s2):
+        distances_ = [i2+1]
+        for i1, c1 in enumerate(s1):
+            if c1 == c2:
+                distances_.append(distances[i1])
+            else:
+                distances_.append(1 + min((distances[i1], distances[i1 + 1], distances_[-1])))
+        distances = distances_
+    return distances[-1]
