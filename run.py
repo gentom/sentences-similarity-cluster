04b247894c29674b95d84c23ffd0c0b56a7ab7af
gentom <gento.m.developer@gmail.com>
2017-09-28 22:41:19 +0900

    commit

---
+++ run.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from levenshteinDistance import editDistance
+
+# これですでにクラスタリングできている　matplotlibに表示する
+def main(datafile):
+    list = []
+    data_list = [line.rstrip('\n') for line in open(datafile)] # ファイルから一行ずつ読み込み、リストにinsert
+    #print(data_list)
+    for i in range(0, len(data_list)):
+        pivot = data_list[i]
+        for j in range(i+1, len(data_list)):
+            ret = editDistance(pivot, data_list[j])
+            print('n{}, n{} : {}'.format(i, j, ret))
+            list.append(ret)
+    print('Result: {}'.format(list))
+
+
+main('./dataset/dummydata.csv')
